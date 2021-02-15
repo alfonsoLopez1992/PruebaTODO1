32abe26bec1766ff0ce1b8fe8b87a155e65c069b
alfonsoLopez1992 <viclopalfguz@gmail.com>
2021-02-15 18:27:09 -0500

    inicial

---
+++ hulk-store-internet/src/main/java/com/hulk/store/web/app/HulkStoreInternetApplication.java
@@ -0,0 +1,13 @@
+package com.hulk.store.web.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HulkStoreInternetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HulkStoreInternetApplication.class, args);
+	}
+
+}
---
+++ hulk-store-internet/src/main/java/com/hulk/store/web/app/models/dao/IUsuarioDao.java
@@ -0,0 +1,10 @@
+package com.hulk.store.web.app.models.dao;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.hulk.store.web.app.models.entity.Usuario;
+
+public interface IUsuarioDao extends CrudRepository<Usuario, Long> {
+	
+
+}
